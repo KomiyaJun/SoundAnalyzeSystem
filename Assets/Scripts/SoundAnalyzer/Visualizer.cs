b5a5b0b5da8fa50438c049bfd3f8e939d0804aaa
KomiyaJun <sujurx.gmail.com>
2026-02-12 04:56:21 +0900

    add: FFT解析を利用したオブジェクトスケーリングのテスト

--- Assets/Scripts/SoundAnalyzer/Visualizer.cs
+++ Assets/Scripts/SoundAnalyzer/Visualizer.cs
@@ -3,20 +3,39 @@ using UnityEngine;
 public class Visualizer : MonoBehaviour
 {
     [SerializeField] int minIndex = 0;
-    [SerializeField] int maxIndex = 512;
+    [SerializeField] int maxIndex = 64;
 
-    private float multiple = 100;
+    [Header("Sensitivity")]
+    [SerializeField] private float baseMultiple = 1000;
+    [SerializeField] private float highFreqBoost = 10f;
+
+    [Header("VisualSmooth")]
+    [SerializeField] private float lerpSpeed = 15f;
+    [SerializeField] private float minHeight = 0.2f;
+
+    private float currentYScale = 0f;
     void Update()
     {
         var analyzer = AudioAnalyzeService.Instance;
 
-        if(analyzer != null)
-        {
-            float vol = analyzer.GetBandAverage(minIndex, maxIndex);
-            
-            Vector3 originScale = gameObject.transform.localScale;
+        float rawVol = analyzer.GetBandAverage(minIndex, maxIndex);
 
-            gameObject.transform.localScale = new Vector3(originScale.x, vol * 10000, originScale.z);
+        float freqPosition = (float)minIndex / 512f;
+        float boost = 1f + (freqPosition * highFreqBoost);
+        float targetVol = rawVol * baseMultiple * boost;
+
+        if(targetVol > currentYScale)
+        {
+            currentYScale = targetVol;
         }
+        else
+        {
+            currentYScale = Mathf.Lerp(currentYScale, targetVol, Time.deltaTime * lerpSpeed);   //Timeégóp
+        }
+
+        float finalY = Mathf.Max(minHeight, currentYScale);
+        Vector3 originalScale = transform.localScale;
+        transform.localScale = new Vector3(originalScale.x, finalY, originalScale.z);
+
     }
 }
